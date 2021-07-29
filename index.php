d795bb1c2c6b02448a54a0b9ec4543dcd26bd9b3
JoaquinMartinezBenjamin <joaquin.martinez.benjamin@gmail.com>
2021-07-29 14:41:39 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+  <title>Animated SVG Avatar </title>
+ 
+<link rel="stylesheet" href="style.css">
+<script  src="script.js"></script>
+
+</head>
+<body>
+<div id="form">
+
+<div class="fish" id="fish"></div>
+<div class="fish" id="fish2"></div>
+
+<form id="waterform" method="post">
+
+<div class="formgroup" id="name-form">
+    <label for="name">Your name*</label>
+    <input type="text" id="name" name="name" />
+</div>
+
+<div class="formgroup" id="email-form">
+    <label for="email">Your e-mail*</label>
+    <input type="email" id="email" name="email" />
+</div>
+
+<div class="formgroup" id="message-form">
+    <label for="message">Your message</label>
+    <textarea id="message" name="message"></textarea>
+</div>
+
+	<input type="submit" value="Send your message!" />
+</form>
+</div>
+
+
+</body>
+</html>
